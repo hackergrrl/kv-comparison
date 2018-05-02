020137db777cdd69456ef2d8de7a398fd9abdb7d
noffle <sww@eight45.net>
2018-05-02 14:53:40 -0700

    add mh versions for 1000 and 10k

--- bench/multi-hypercore.js
+++ bench/multi-hypercore.js
@@ -25,3 +25,47 @@ bench('multi-hypercore/ram/insert & index 100 keys', function (b) {
     }
   })
 })
+
+bench('multi-hypercore/ram/insert & index 1000 keys', function (b) {
+  var db = hyperkv(ram, memdb())
+
+  db.ready(function () {
+    b.start()
+
+    var pending = 1000
+    for (var i = 0; i < 1000; i++) {
+      var key = randomBytes(16).toString('hex')
+      var value = randomBytes(32).toString('hex')
+      db.put(key, value, done)
+    }
+
+    function done () {
+      if (--pending) return
+      db.index.ready(function () {
+        b.end()
+      })
+    }
+  })
+})
+
+bench('multi-hypercore/ram/insert & index 10000 keys', function (b) {
+  var db = hyperkv(ram, memdb())
+
+  db.ready(function () {
+    b.start()
+
+    var pending = 10000
+    for (var i = 0; i < 10000; i++) {
+      var key = randomBytes(16).toString('hex')
+      var value = randomBytes(32).toString('hex')
+      db.put(key, value, done)
+    }
+
+    function done () {
+      if (--pending) return
+      db.index.ready(function () {
+        b.end()
+      })
+    }
+  })
+})

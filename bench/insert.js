7936082704866977b38e39048ea090e244ea3e75
noffle <sww@eight45.net>
2018-05-03 10:42:34 -0700

    add disk benches

--- bench/insert.js
+++ bench/insert.js
@@ -4,6 +4,8 @@ var ram = require('random-access-memory')
 var randomBytes = require('randombytes')
 var memdb = require('memdb')
 var hyperkv = require('../multi-kv')
+var tmp = require('tmp').tmpNameSync
+var level = require('level')
 
 bench('hyperdb/ram/insert & index 100 keys', function (b) {
   var db = hyperdb(ram, { valueEncoding: 'json' })
@@ -59,7 +61,59 @@ bench('hyperdb/ram/insert & index 10000 keys', function (b) {
   }
 })
 
+bench('hyperdb/disk/insert & index 100 keys', function (b) {
+  var db = hyperdb(tmp(), { valueEncoding: 'json' })
 
+  b.start()
+
+  var pending = 100
+  for (var i = 0; i < 100; i++) {
+    var key = randomBytes(16).toString('hex')
+    var val = randomBytes(32).toString('hex')
+    db.put(key, val, done)
+  }
+
+  function done () {
+    if (--pending) return
+    b.end()
+  }
+})
+
+bench('hyperdb/disk/insert & index 1000 keys', function (b) {
+  var db = hyperdb(tmp(), { valueEncoding: 'json' })
+
+  b.start()
+
+  var pending = 1000
+  for (var i = 0; i < 1000; i++) {
+    var key = randomBytes(16).toString('hex')
+    var val = randomBytes(32).toString('hex')
+    db.put(key, val, done)
+  }
+
+  function done () {
+    if (--pending) return
+    b.end()
+  }
+})
+
+bench('hyperdb/disk/insert & index 10000 keys', function (b) {
+  var db = hyperdb(tmp(), { valueEncoding: 'json' })
+
+  b.start()
+
+  var pending = 10000
+  for (var i = 0; i < 10000; i++) {
+    var key = randomBytes(16).toString('hex')
+    var val = randomBytes(32).toString('hex')
+    db.put(key, val, done)
+  }
+
+  function done () {
+    if (--pending) return
+    b.end()
+  }
+})
 
 bench('multi-hypercore/ram/insert & index 100 keys', function (b) {
   var db = hyperkv(ram, memdb())
@@ -107,6 +161,72 @@ bench('multi-hypercore/ram/insert & index 1000 keys', function (b) {
 
 bench('multi-hypercore/ram/insert & index 10000 keys', function (b) {
   var db = hyperkv(ram, memdb())
+
+  db.ready(function () {
+    b.start()
+
+    var pending = 10000
+    for (var i = 0; i < 10000; i++) {
+      var key = randomBytes(16).toString('hex')
+      var value = randomBytes(32).toString('hex')
+      db.put(key, value, done)
+    }
+
+    function done () {
+      if (--pending) return
+      db.index.ready(function () {
+        b.end()
+      })
+    }
+  })
+})
+
+bench('multi-hypercore/disk/insert & index 100 keys', function (b) {
+  var db = hyperkv(tmp(), level(tmp()))
+
+  db.ready(function () {
+    b.start()
+
+    var pending = 100
+    for (var i = 0; i < 100; i++) {
+      var key = randomBytes(16).toString('hex')
+      var value = randomBytes(32).toString('hex')
+      db.put(key, value, done)
+    }
+
+    function done () {
+      if (--pending) return
+      db.index.ready(function () {
+        b.end()
+      })
+    }
+  })
+})
+
+bench('multi-hypercore/disk/insert & index 1000 keys', function (b) {
+  var db = hyperkv(tmp(), level(tmp()))
+
+  db.ready(function () {
+    b.start()
+
+    var pending = 1000
+    for (var i = 0; i < 1000; i++) {
+      var key = randomBytes(16).toString('hex')
+      var value = randomBytes(32).toString('hex')
+      db.put(key, value, done)
+    }
+
+    function done () {
+      if (--pending) return
+      db.index.ready(function () {
+        b.end()
+      })
+    }
+  })
+})
+
+bench('multi-hypercore/disk/insert & index 10000 keys', function (b) {
+  var db = hyperkv(tmp(), level(tmp()))
 
   db.ready(function () {
     b.start()
--- bench/lookup.js
+++ bench/lookup.js
@@ -4,9 +4,11 @@ var ram = require('random-access-memory')
 var randomBytes = require('randombytes')
 var memdb = require('memdb')
 var hyperkv = require('../multi-kv')
+var tmp = require('tmp').tmpNameSync
+var level = require('level')
 
 bench('hyperdb/ram/lookup 1000 keys vs 1000 entries', function (b) {
-  var db = hyperdb(ram, { valueEncoding: 'json' })
+  var db = hyperdb(tmp(), { valueEncoding: 'json' })
 
   var pending = 1000
   var keys = {}
@@ -37,6 +39,71 @@ bench('hyperdb/ram/lookup 1000 keys vs 1000 entries', function (b) {
 
 bench('multi-hypercore/ram/lookup 1000 keys vs 1000 entries', function (b) {
   var db = hyperkv(ram, memdb())
+
+  db.ready(function () {
+    var pending = 1000
+    var keys = {}
+    for (var i = 0; i < 1000; i++) {
+      var key = randomBytes(16).toString('hex')
+      var val = randomBytes(32).toString('hex')
+      keys[key] = val
+      db.put(key, val, done)
+    }
+
+    function done () {
+      if (--pending) return
+
+      db.index.ready(function () {
+        b.start()
+
+        ;(function next (n) {
+          if (!n) { return b.end() }
+          var kk = Object.keys(keys)
+          var key = kk[Math.floor(Math.random() * kk.length)]
+          var expected = keys[key]
+          db.get(key, function (_, values) {
+            var val = values[0]
+            if (expected != val) throw Error('its broke')
+            next(--n)
+          })
+        })(1000)
+      })
+    }
+  })
+})
+
+bench('hyperdb/disk/lookup 1000 keys vs 1000 entries', function (b) {
+  var db = hyperdb(ram, { valueEncoding: 'json' })
+
+  var pending = 1000
+  var keys = {}
+  for (var i = 0; i < 1000; i++) {
+    var key = randomBytes(16).toString('hex')
+    var val = randomBytes(32).toString('hex')
+    keys[key] = val
+    db.put(key, val, done)
+  }
+
+  function done () {
+    if (--pending) return
+    b.start()
+
+    ;(function next (n) {
+      if (!n) { return b.end() }
+      var kk = Object.keys(keys)
+      var key = kk[Math.floor(Math.random() * kk.length)]
+      var expected = keys[key]
+      db.get(key, function (_, nodes) {
+        var val = nodes[0].value
+        if (expected != val) throw Error('its broke')
+        next(--n)
+      })
+    })(1000)
+  }
+})
+
+bench('multi-hypercore/disk/lookup 1000 keys vs 1000 entries', function (b) {
+  var db = hyperkv(tmp(), level(tmp()))
 
   db.ready(function () {
     var pending = 1000
